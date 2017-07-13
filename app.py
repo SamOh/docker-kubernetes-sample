2ac784df262eecd5a7608278e5d39225948e49d5
ssoh <samuel.s.oh@jpl.nasa.gov>
2017-07-13 10:30:12 -0700

    edited a few things to make it open to public

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 # extremely simple flask app
 @app.route('/', methods=['GET'])
 def home():
-    return 'Go TPT!'
+    return 'Lets learn docker!'
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
